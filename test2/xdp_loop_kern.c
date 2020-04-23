9f1ff43e8cc5f3485ee1550ca776b3b8ecf00f00
Till Müller <till.mueller@tum.de>
2020-04-23 14:29:09 +0200

    Minor fixes, O3 to unroll loops more

--- test2/xdp_loop_kern.c
+++ test2/xdp_loop_kern.c
@@ -6,18 +6,18 @@ SEC("xdp")
 int  xdp_prog_loop(struct xdp_md *ctx) {
 	unsigned char* data = (void *)(long)ctx->data;
 	unsigned char* data_end = (void *)(long)ctx->data_end;
-	int length = data_end - data;
+
 	unsigned char xor;
-	for(int i = 0; i < length; i++) {
+	for(int i = 0; i < 60; i++) {
 		if(data + i > data_end)
-			return XDP_DROP;
+			return XDP_ABORT;
 		xor ^= data[i];
 	}
 
 	unsigned char tmp;
 	for(int i = 0; i < 6; i++) {
 		if(data + i + 8 > data_end)
-			return XDP_DROP;
+			return XDP_ABORT;
 		tmp = data[i];
 		data[i] = data[i + 6];
 		data[i + 6] = tmp;
