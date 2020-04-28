786dc80cdb527d07b2c248394572fca002b994e6
Till Müller <till.mueller@tum.de>
2020-04-28 14:12:54 +0200

    Added test6 for even more optimization

---
+++ test6/xdp_loop_kern-template.c
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+#include <bpf_helpers.h>
+
+#define BYTES /*{%BYTE_COUNT%}*/0
+
+struct bpf_map_def SEC("maps") xdp_loop_map = {
+	.type        = BPF_MAP_TYPE_PERCPU_ARRAY,
+	.key_size    = sizeof(int),
+	.value_size  = sizeof(unsigned char),
+	.max_entries = 1,
+};
+
+
+SEC("xdp")
+int  xdp_prog_loop(struct xdp_md *ctx) {
+	unsigned char* data = (void *)(long)ctx->data;
+	unsigned char* data_end = (void *)(long)ctx->data_end;
+
+	if(data + BYTES > data_end)
+		return XDP_ABORTED;
+
+	int key = 0;
+	unsigned char* val = bpf_map_lookup_elem(&xdp_loop_map, &key);
+	if(!val)
+		return XDP_ABORTED;
+
+	#pragma unroll
+	for(int i = 0; i < BYTES; i++) {
+		*val = data[i];
+	}
+
+	unsigned char tmp;
+	for(int i = 0; i < 6; i++) {
+		if(data + i + 8 > data_end)
+			return XDP_ABORTED;
+		tmp = data[i];
+		data[i] = data[i + 6];
+		data[i + 6] = tmp;
+	}
+	return XDP_TX;
+}
+
+char _license[] SEC("license") = "GPL";
