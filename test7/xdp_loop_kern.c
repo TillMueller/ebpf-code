2b6d66be48f36af157d7268ae7ae4722720cdb44
Till Müller <till.mueller@tum.de>
2020-04-30 14:50:48 +0200

    add 8th test to compare MAP_TYPE_ARRAY and MAP_TYPE_HASH - probably will not work in the current state

---
+++ test7/xdp_loop_kern.c
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+#include <bpf_helpers.h>
+
+#ifndef BYTES
+#define BYTES 0
+#endif
+
+struct bpf_map_def SEC("maps") xdp_loop_map = {
+	.type        = BPF_MAP_TYPE_PERCPU_HASH,
+	.key_size    = sizeof(int),
+	.value_size  = sizeof(unsigned char),
+	.max_entries = BYTES,
+};
+
+SEC("xdp")
+int  xdp_prog_loop(struct xdp_md *ctx) {
+	unsigned char* data = (void *)(long)ctx->data;
+	unsigned char* data_end = (void *)(long)ctx->data_end;
+
+	#pragma unroll
+	for(int i = 0; i < BYTES; i++) {
+		if(data + i + 8 > data_end)
+			return XDP_ABORTED;
+		int tmp = i;
+		unsigned char* val = bpf_map_lookup_elem(&xdp_loop_map, &tmp);
+		if(!val)
+			return XDP_ABORTED;
+		*val = data[i];
+	}
+
+	unsigned char tmp;
+	for(int i = 0; i < 6; i++) {
+		if(data + i + 8 > data_end)
+			return XDP_ABORTED;
+		tmp = data[i];
+		data[i] = data[i + 6];
+		data[i + 6] = tmp;
+	}
+	return XDP_TX;
+}
+
+char _license[] SEC("license") = "GPL";
