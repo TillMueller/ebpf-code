4b7c27fdf93981cc222d1f100ac432c8089f7e7d
Till Müller <till.mueller@tum.de>
2020-05-03 16:21:15 +0200

    simplify test10

--- test10/write_stats.c
+++ test10/write_stats.c
@@ -2,7 +2,6 @@
 #include <unistd.h>
 #include <bpf.h>
 #include <libbpf.h>
-#include "common_kern_user.h"
 
 #define MAX_PATH_LENGTH 4096
 
@@ -18,15 +17,15 @@ int main(int argc, char* argv[]) {
     }
     for(;;) {
         usleep(1000000);
-        __u32 key = XDP_PASS;
-        struct datarec value;
+        int key = 0;
+        int value;
         int error = bpf_map_lookup_elem(mapfd, &key, &value);
         if(error != 0) {
             printf("Could not get value from map, exiting\n");
             fflush(stdout);
             return 1;
         }
-        printf("packets: %lld\n", value.rx_packets);
+        printf("packets: %lld\n", value);
         fflush(stdout);
     }
 }
--- test10/xdp_prog_kern.c
+++ test10/xdp_prog_kern.c
@@ -7,7 +7,7 @@
 struct bpf_map_def __attribute__ ((section ("maps"))) xdp_stats_map = {
 	.type        = BPF_MAP_TYPE_ARRAY,
 	.key_size    = sizeof(int),
-	.value_size  = sizeof(struct datarec),
+	.value_size  = sizeof(int),
 	.max_entries = 1,
 };
 
@@ -15,13 +15,12 @@ __attribute__ ((section ("xdp_stats1")))
 int  xdp_stats1_func(struct xdp_md *ctx) {
 	unsigned char* data = (void *)(long)ctx->data;
 	unsigned char* data_end = (void *)(long)ctx->data_end;
-	struct datarec *rec;
 	int key = 0;
-	rec = bpf_map_lookup_elem(&xdp_stats_map, &key);
-	if (!rec)
+	int* val = bpf_map_lookup_elem(&xdp_stats_map, &key);
+	if (!val)
 		return XDP_ABORTED;
 
-	__sync_fetch_and_add(&rec->rx_packets, 1);
+	__sync_fetch_and_add(val, 1);
 
 	unsigned char tmp;
 	for(int i = 0; i < 6; i++) {
