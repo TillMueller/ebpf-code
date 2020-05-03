dbd650c1c3c93eaa691a20f0318b06e5f12e28ac
Till Müller <till.mueller@tum.de>
2020-05-03 10:44:42 +0200

    Added new test for BPF_MAP_TYPE_HASH, fixed descriptions of test7 and test8

---
+++ test9/xdp_loop_kern.c
@@ -0,0 +1,47 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+#include <bpf_helpers.h>
+
+#ifndef BYTES
+#define BYTES 0
+#endif
+
+struct bpf_map_def SEC("maps") xdp_loop_map = {
+	.type        = BPF_MAP_TYPE_HASH,
+	.key_size    = sizeof(int),
+	.value_size  = sizeof(unsigned char),
+	.max_entries = BYTES,
+};
+
+SEC("xdp")
+int  xdp_prog_loop(struct xdp_md *ctx) {
+	unsigned char* data = (void *)(long)ctx->data;
+	unsigned char* data_end = (void *)(long)ctx->data_end;
+
+	unsigned char init_val, key = 0;
+	unsigned char* val = bpf_map_lookup_elem(&xdp_loop_map, &key);
+	if(!val) {
+		bpf_map_update_elem(&xdp_loop_map, &key, &init_val, BPF_ANY);
+		if(!val)
+			return XDP_ABORTED;
+	}
+
+	#pragma unroll
+	for(int i = 1; i <= BYTES; i++) {
+		if(data + i > data_end)
+			return XDP_ABORTED;
+		*val = data[i - 1];
+	}
+
+	unsigned char tmp;
+	for(int i = 0; i < 6; i++) {
+		if(data + i + 8 > data_end)
+			return XDP_ABORTED;
+		tmp = data[i];
+		data[i] = data[i + 6];
+		data[i + 6] = tmp;
+	}
+	return XDP_TX;
+}
+
+char _license[] SEC("license") = "GPL";
