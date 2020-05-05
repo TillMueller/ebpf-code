cbefb2c01677317a1e10f103ceb71f7f133bfdb3
Till Müller <till.mueller@tum.de>
2020-05-05 12:13:50 +0200

    moved test10 to test1, removed test11 since it is equal to test0

--- test1/xdp_prog_kern.c
+++ test1/xdp_prog_kern.c
@@ -10,23 +10,12 @@ struct bpf_map_def SEC("maps") xdp_stats_map = {
 
 SEC("xdp_stats1")
 int  xdp_stats1_func(struct xdp_md *ctx) {
-	unsigned char* data = (void *)(long)ctx->data;
-	unsigned char* data_end = (void *)(long)ctx->data_end;
 	int key = 0;
 	int* val = bpf_map_lookup_elem(&xdp_stats_map, &key);
 	if (!val)
 		return XDP_ABORTED;
 
 	__sync_fetch_and_add(val, 1);
-
-	unsigned char tmp;
-	for(int i = 0; i < 6; i++) {
-		if(data + i + 8 > data_end)
-			return XDP_DROP;
-		tmp = data[i];
-		data[i] = data[i + 6];
-		data[i + 6] = tmp;
-	}
 	return XDP_TX;
 }
 
